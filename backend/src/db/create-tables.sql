f7ed23f161e971b65aaac2427ae5ca639dc25457
Eppu Peltonen <22355166+eppu-peltonen@users.noreply.github.com>
2024-09-06 13:23:24 +0300

    clean

--- backend/src/db/create-tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    passwordHash VARCHAR(255) NOT NULL,
-    memberId UUID NOT NULL,
-    createdAt TIMESTAMP NOT NULL
-);

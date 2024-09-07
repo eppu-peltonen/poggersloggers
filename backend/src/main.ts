5c0e70babf6c59b68b90bbca0576bd3a04ab5d08
Eppu Peltonen <22355166+eppu-peltonen@users.noreply.github.com>
2024-09-07 21:12:07 +0300

    Dockerize nestjs postgres and adminer

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = app.get(ConfigService).get<number>("API_PORT");
+  await app.listen(port);
 }
 bootstrap();

709c9116758ffbcdafaa251baf16c9ac2642996e
Eppu Peltonen <22355166+eppu-peltonen@users.noreply.github.com>
2024-09-08 13:28:28 +0300

    fix devcontainers

--- backend/src/app/app.controller.ts
+++ backend/src/app/app.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Controller, Get} from "@nestjs/common";
 import { AppService } from "./app.service";
-import { AuthGuard } from "src/auth/auth.guard";
 
 @Controller("api")
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
-    @UseGuards(AuthGuard)
     @Get("hello")
     getHello(): string {
         return this.appService.getHello();
